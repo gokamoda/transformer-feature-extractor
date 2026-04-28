f696c7c4ea402404b395f3ff4f0c801e86dec20a
gokamoda <gokamoda@gmail.com>
2026-04-28 22:25:05 +0900

    added device management

--- src/feature_extractor/reconstruction/rope.py
+++ src/feature_extractor/reconstruction/rope.py
@@ -6,10 +6,11 @@ from feature_extractor.typing import Tensor
 class SimplifiedRoPEV1:
     def __init__(self, inv_freq: Tensor, attention_scaling: float = 1.0):
         self.inv_freq = inv_freq.to(torch.float32)
+        self.device = self.inv_freq.device
         self.attention_scaling = attention_scaling
 
     def create_position_embeddings(self, sequence_length: int):
-        position_ids = torch.arange(sequence_length).unsqueeze(0)
+        position_ids = torch.arange(sequence_length).unsqueeze(0).to(self.device)
 
         inv_freq_expanded = (
             self.inv_freq[None, :, None].float().expand(position_ids.shape[0], -1, 1)
